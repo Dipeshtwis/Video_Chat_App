1fe34d0f99c2730582ba8cb8d46e1d96f7953e53
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-01 22:24:41 +0530

    Add user connected message

--- server.js
+++ server.js
@@ -15,4 +15,11 @@ app.get('/:room', (req, res) => {
 	res.render('room', {roomId: req.params.room})
 })
 
+io.on('connection', socket => {
+	socket.on('join-room', (roomId, userId) => {
+		socket.join(roomId);
+		socket.to(roomId).broadcast.emit('user-connected', userId);
+	})
+})
+
 server.listen(3000);
