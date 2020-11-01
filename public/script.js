1b1fb4de366277922c29c70bc0dc9aa890ffe487
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-02 00:55:53 +0530

    End video call functionality

--- public/script.js
+++ public/script.js
@@ -14,16 +14,30 @@ navigator.mediaDevices.getUserMedia({
 	audio: true
 }).then(stream => {
 	addVideoStream(myVideo, stream);
+
+	myPeer.on('call', call => {
+		call.answer(stream);
+	})
+
+	socket.on('user-connected', userId => {
+		connectToNewUser(userId, stream)
+	})
 })
 
 myPeer.on('open', id => {
 	socket.emit('join-room', ROOM_ID, id);
 })
 
-
-socket.on('user-connected0', userId => {
-	console.log('User-connected:' + userId);
-}) 
+function connectToNewUser(userId, stream){
+	const call = myPeer.call(userId, stream);
+	const video = document.createElement('video');
+	call.on('stream', userVideoStream => {
+		addVideoStream(video, userVideoStream);
+	})
+	call.on('close', () => {
+		video.remove();
+	})
+}
 
 function addVideoStream(video, stream) {
 	video.srcObject = stream;
