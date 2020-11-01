100e464f3345abbc3ab155ee1804322b51b4c5a1
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-02 00:31:28 +0530

    Add unique-id for unique user

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 const socket = io('/');
 
-socket.emit('join-room', ROOM_ID, 10);
+const myPeer = new Peer(undefined, {
+	host: '/', 
+	port: '3001'
+})
+
+myPeer.on('open', id => {
+	socket.emit('join-room', ROOM_ID, id);
+})
+
 
 socket.on('user-connected0', userId => {
 	console.log('User-connected:' + userId);
-})
+}) 
